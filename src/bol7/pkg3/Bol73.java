b0daa7b78cfd4ef2390566ffc29641ca2d1d2466
galle <galle@DESKTOP-MFD5V2L>
2015-11-03 13:28:34 +0000

    Ejercicio terminado

---
+++ src/bol7/pkg3/Bol73.java
@@ -0,0 +1,16 @@
+package bol7.pkg3;
+
+/**
+ *
+ * @author galle
+ */
+public class Bol73 {
+
+    public static void main(String[] args) {
+       float num1;
+       Comparacion obj1 = new Comparacion ();
+       num1 = obj1.pedirNumero();
+       obj1.compara(num1);
+    }
+    
+}
